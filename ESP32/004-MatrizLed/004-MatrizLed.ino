c16f30be78c8d04847be440a58015ace6a1db56a
Miguel Cano Santana <55782155+miguelcanosantana@users.noreply.github.com>
2025-02-06 17:03:35 +0100

    Update 004-MatrizLed.ino

--- ESP32/004-MatrizLed/004-MatrizLed.ino
+++ ESP32/004-MatrizLed/004-MatrizLed.ino
@@ -2,20 +2,23 @@ byte ledPins[] = {15, 2, 0, 4, 5, 18, 19, 21, 22, 23};
 int ledCounts;
 
 void setup() {
-  
+
      ledCounts = sizeof(ledPins);
-     for (int i = 0; i < ledCounts; i++) {
+
+     for (int i = 0; i < sizeof(ledPins); i++) {
           pinMode(ledPins[i], OUTPUT);
      }
 }
 
 void loop() {
-     for (int i = 0; i < ledCounts; i++) {
+
+     for (int i = 0; i < ledCounts - 1; i++) {
           digitalWrite(ledPins[i], HIGH);
           delay(100);
           digitalWrite(ledPins[i], LOW);
      }
-     for (int i = ledCounts - 1; i > -1; i--) {
+     
+     for (int i = ledCounts - 1; i > 0; i--) {
           digitalWrite(ledPins[i], HIGH);
           delay(100);
           digitalWrite(ledPins[i], LOW);
